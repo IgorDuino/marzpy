a57c3ecefe12b95387e0c0a803a276374b0517f2
Igor Kuzmenkov <tkdomigor@gmail.com>
2025-07-21 20:57:16 +0300

    fix autodelete

--- marzpy/api/user.py
+++ marzpy/api/user.py
@@ -1,17 +1,19 @@
 from .send_requests import *
 
-async def delete_if_exist(dic,keys:list):
+
+async def delete_if_exist(dic, keys: list):
     for key in keys:
         if key in dic:
             del dic[key]
     return dic
+
 
 class User:
     def __init__(
         self,
         username: str,
         proxies: dict,
-        inbounds: dict,  
+        inbounds: dict,
         data_limit: float,
         data_limit_reset_strategy: str = "no_reset",
         status="",
@@ -22,16 +24,14 @@ class User:
         links=[],
         subscription_url="",
         excluded_inbounds={},
-        note = "",
-        on_hold_timeout= 0,
-        on_hold_expire_duration = 0,
-        sub_updated_at = 0,
-        online_at = 0,
-        sub_last_user_agent:str = "",
-        auto_delete_in_days:int = 0,
-        admin=None,
-        **_, # Stop causing error when marzban update
-        
+        note="",
+        on_hold_timeout=0,
+        on_hold_expire_duration=0,
+        sub_updated_at=0,
+        online_at=0,
+        sub_last_user_agent: str = "",
+        auto_delete_in_days: int = 0,
+        **kwargs,
     ):
         self.username = username
         self.proxies = proxies
@@ -53,8 +53,35 @@ class User:
         self.online_at = online_at
         self.sub_updated_at = sub_updated_at
         self.auto_delete_in_days = auto_delete_in_days
-        self.admin = admin
-        
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        """Returns a readable string representation of the User object"""
+        attrs = [
+            f"username='{self.username}'",
+            f"status='{self.status}'",
+            f"data_limit={self.data_limit}",
+            f"expire={self.expire}",
+            f"used_traffic={self.used_traffic}",
+            f"lifetime_used_traffic={self.lifetime_used_traffic}",
+            f"created_at='{self.created_at}'",
+            f"subscription_url='{self.subscription_url}'",
+            f"note='{self.note}'",
+            f"on_hold_timeout={self.on_hold_timeout}",
+            f"on_hold_expire_duration={self.on_hold_expire_duration}",
+            f"sub_updated_at={self.sub_updated_at}",
+            f"online_at={self.online_at}",
+            f"auto_delete_in_days={self.auto_delete_in_days}",
+        ]
+        return f"User({', '.join(attrs)})"
+
+    def __repr__(self):
+        """Returns the same as __str__"""
+        return self.__str__()
+
+
 class UserMethods:
     async def add_user(self, user: User, token: dict):
         """add new user.
@@ -99,7 +126,9 @@ class UserMethods:
 
         Returns: `~User`: api.User object
         """
-        request = await send_request(f"user/{user_username}", token, "put", user.__dict__)
+        request = await send_request(
+            f"user/{user_username}", token, "put", user.__dict__
+        )
         return User(**request)
 
     async def delete_user(self, user_username: str, token: dict):
@@ -127,7 +156,7 @@ class UserMethods:
         """
         await send_request(f"user/{user_username}/reset", token, "post")
         return "success"
-    
+
     async def revoke_sub(self, user_username: str, token: dict):
         """Revoke users subscription (Subscription link and proxies) traffic by username.
 
@@ -140,7 +169,7 @@ class UserMethods:
         """
         request = await send_request(f"user/{user_username}/revoke_sub", token, "post")
         return User(**request)
-    
+
     async def get_all_users(self, token: dict, username=None, status=None):
         """get all users list.
 
@@ -195,7 +224,9 @@ class UserMethods:
 
         Returns: `~dict`: dict of user usage
         """
-        return (await send_request(f"user/{user_username}/usage", token, "get"))['usages'][0]
+        return (await send_request(f"user/{user_username}/usage", token, "get"))[
+            "usages"
+        ][0]
 
     async def get_all_users_count(self, token: dict):
         """get all users count.
